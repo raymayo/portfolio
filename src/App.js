6347947d5e3055528a3d1d9dfefb0ce6d5d54e3e
Ray Mayo <ormillada.jr@gmail.com>
2022-05-04 15:29:08 +0800

    added resize

--- src/App.js
+++ src/App.js
@@ -13,13 +13,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
     const [loading, setLoading] = useState(false);
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
 
     useEffect(() => {
         setLoading(true);
         setTimeout(() => {
             setLoading(false)
         }, 2000)
-    }, [])
+    }, []);
+
     return (
         <BrowserRouter>
             <div id="webContainer">
