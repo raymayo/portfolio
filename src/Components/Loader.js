71ebba1e27e680cfe0749553d2e357a67781685f
Ray Mayo <ormillada.jr@gmail.com>
2022-05-04 21:51:53 +0800

    added loader

---
+++ src/Components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import loaderCss from '../Stylesheets/loader.module.css'
+
+const Loader = () => {
+    return (
+        <div id={loaderCss.loaderContainer}>
+            Loader
+        </div>
+    )
+}
+
+export default Loader
