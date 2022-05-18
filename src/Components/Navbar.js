60432046f4f0ae48f771eb5ee13e63216252f24b
Ray Mayo <ormillada.jr@gmail.com>
2022-05-18 17:02:42 +0800

    download

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { motion } from 'framer-motion'
-import navCss from '../Stylesheets/navBar.module.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import navCss from '../Stylesheets/navBar.module.css';
 import { ReactComponent as Logo } from '../logo.svg';
+import { saveAs } from 'file-saver';
+
+
 
 const Navbar = () => {
+
+    const downloadImage = () => {
+        saveAs('../Ray-Mayo-Resume.pdf', 'Ray-Mayo-Resume.pdf')
+    }
+
     return (
         <motion.div id={navCss.headerContainer} initial={{ y: -100 }} animate={{ y: 0 }} transition={{ type: 'spring', ease: 'easeInOut' }}>
             <Link id={navCss.logoLink} to="/portfolio">
                 <Logo id={navCss.Logo} />
             </Link>
-            <a href="RayMayo-Resume.png" id={navCss.resume} className={navCss.download} download>RESUME</a>
+            <span id={navCss.resume} className={navCss.download} onClick={downloadImage}>RESUME</span>
         </motion.div>
     )
 }
