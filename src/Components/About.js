1809412ec45b30ed249a4e6195b56f64a32895fe
Ray Mayo <ormillada.jr@gmail.com>
2022-05-31 19:34:32 +0800

    added link

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,40 +13,45 @@ const About = () => {
             <div id={aboutCss.content}>
 
                 <div id={aboutCss.leftContainer}>
-                    <div id={aboutCss.titleContainer}>
-                        <h1>RAY</h1>
-                        <h1>MAYO</h1>
-                    </div>
-                    <div id={aboutCss.descBox}>
-                        <h2>I'm a 20 y/o self-taught graphic designer and front end developer based in the Philippines.</h2>
-                    </div>
-                    <div id={aboutCss.linkContainer}>
-                        <FB className={aboutCss.svgLink} />
-                        <INSTA className={aboutCss.svgLink} />
-                        <GIT className={aboutCss.svgLink} />
-                        <LINKED className={aboutCss.svgLink} />
+                    <div id={aboutCss.infoBox}>
+                        <div id={aboutCss.titleContainer}>
+                            <h1>RAY</h1>
+                            <h1>MAYO</h1>
+                        </div>
+                        <div id={aboutCss.descBox}>
+                            <h1>I'm a 20 y/o self-taught graphic designer and front end developer based in the Philippines.</h1>
+                        </div>
+                        <div id={aboutCss.linkContainer}>
+                            <FB className={aboutCss.svgLink} />
+                            <INSTA className={aboutCss.svgLink} />
+                            <GIT className={aboutCss.svgLink} />
+                            <LINKED className={aboutCss.svgLink} />
+                        </div>
                     </div>
                 </div>
 
-                <div id={aboutCss.rightContainer}>
-                    <h1>Tools and Skills:</h1>
-                    <div className={aboutCss.skillsTable}>
-                        <div className={aboutCss.skillsTitle}>
-                            <h3 >Design Tools</h3>
-                            <h3>01</h3>
-                        </div>
-                        <div className={aboutCss.list}>
-                            <h4>Photoshop · Illustrator · Adobe XD · Figma · After Effects · Premiere Pro · Canva · InDesign</h4>
-                        </div>
-                    </div>
 
-                    <div className={aboutCss.skillsTable}>
-                        <div className={aboutCss.skillsTitle}>
-                            <h3>Web Dev Tools</h3>
-                            <h3>02</h3>
+                <div id={aboutCss.rightContainer}>
+                    <div id={aboutCss.skillBox}>
+                        <h1 id={aboutCss.toolsTitle}>Tools I Use:</h1>
+                        <div className={aboutCss.skillsTable}>
+                            <div className={aboutCss.skillsTitle}>
+                                <h1 >Design Tools</h1>
+                                <h1>01</h1>
+                            </div>
+                            <div className={aboutCss.list}>
+                                <h4>Photoshop · Illustrator · Adobe XD · Figma · After Effects · Premiere Pro · Canva · InDesign</h4>
+                            </div>
                         </div>
-                        <div className={aboutCss.list}>
-                            <h4>HTML5 · CSS · JavaScript · React Js · Node Js · PHP · Wordpress · Python · Django · MongoDB · SQL</h4>
+
+                        <div className={aboutCss.skillsTable}>
+                            <div className={aboutCss.skillsTitle}>
+                                <h1>Web Dev Tools</h1>
+                                <h1>02</h1>
+                            </div>
+                            <div className={aboutCss.list}>
+                                <h4>HTML5 · CSS · JavaScript · React Js · Node Js · PHP · Wordpress · Python · Django · MongoDB · SQL</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
